85de01eab752585231b41116151afd33ae836034
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2023-10-29 19:18:09 -0300

    p2 e knuth organizados e com saida em arquivo

--- shellsort/shellsort.cpp
+++ shellsort/shellsort.cpp
@@ -23,12 +23,8 @@ int main() {
 
     /*------------------------------------------------------------------------------------------------------*/
 
-    start = clock();
+    cout << "\nSEQ SHELL P2" << endl;
     shell_p2(vetor_copia, tamanho_vetor);
-    end = clock();
-
-    resultado_tempo = (double) (end - start);
-    cout << "(SHELL P2) Tempo de execucao: " << resultado_tempo << " milissegundos" << endl;
 
     /*------------------------------------------------------------------------------------------------------*/
 
@@ -36,20 +32,22 @@ int main() {
       vetor_copia[i] = vetor[i];
     }
 
-    start = clock();
+    cout << "SEQ SHELL KNUTH" << endl;
     shell_knuth(vetor_copia, tamanho_vetor);
-    end = clock();
 
-    resultado_tempo = (double) (end - start);
-    cout << "(SHELL KNUTH) Tempo de execucao: " << resultado_tempo << " milissegundos" << endl;
+    /*------------------------------------------------------------------------------------------------------*/
+
+    cout << "\n";
   }
 }
 
 // SHELL POTÊNCIAS DE DOIS
 void shell_p2(int *vetor, int tamanho) {
-  int h = tamanho / 2;
+  int h;
 
-  while (h >= 1) {
+  for(h = 1; h < tamanho/2; h = h*2);   
+
+  while (h > 0) {
     for (int i = h; i < tamanho; i++) {
       int j = i;
 
@@ -61,33 +59,40 @@ void shell_p2(int *vetor, int tamanho) {
       }
     }
 
+    for(int j = 0; j < tamanho; j++){
+      cout << vetor[j] << " ";
+    }
+    cout << "INCR = " << h << endl;
+
     h /= 2;
   }
 }
 
 // SHELL KNUTH
 void shell_knuth(int *vetor, int tamanho) {
-    int i, j, h;
-    int aux;
+  int h;
+  
+  for(h = 1; h < tamanho; (h = (3 * h) + 1)); 
 
-    for(h = 1; h < tamanho; h = 3*h+1); /* calcula o h inicial. */
+  while(h > 0) {
+    h = (h-1)/3;
+    
+    for(int i = h; i < tamanho; i++) {
+      int j = i;
 
-    while(h > 0) {
-        h = (h-1)/3; /* atualiza o valor de h. */
-        for(i = h; i < tamanho; i++) {
-            aux = vetor[i];
-            j = i;
-
-            /* efetua comparações entre elementos com distância h: */
-            while(vetor[j - h] > aux) {
-                vetor[j] = vetor[j - h];
-                j -= h;
-                if(j < h) break;
-            }
-
-            vetor[j] = aux;
-        }
+      while (j >= h && vetor[j] < vetor[j - h]) {
+        int temp = vetor[j];
+        vetor[j] = vetor[j - h];
+        vetor[j - h] = temp;
+        j -= h;
+      }
     }
+
+    for(int j = 0; j < tamanho; j++){
+      cout << vetor[j] << " ";
+    }
+    cout << "INCR = " << h << endl;
+  }
 }
 
 // SHELL CIURA
@@ -102,6 +107,8 @@ void shell_ciura(int *vetor, int n){
         break;
     }
   }
+
+  // [5 4 27 6 10 3 15 2]
 
   while(h > 0){
     for(int i = h; i < n; i++) {
