037deb3a5c2d223b892b5aaba7d95d11aec8f1e3
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2023-12-08 21:52:24 -0300

    pesquisa de um jogador

--- hashtable/hashtable.cpp
+++ hashtable/hashtable.cpp
@@ -1,19 +1,22 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
 typedef struct{
     int id;
-    string team;
+    string name;
     string position;
 } Players;
 
+void buscaJogador (vector<vector<Players>> hashtable, int id);
+
 int main(){
+    // Declarando os dados
     vector<vector<Players>> hashtable(50);
     Players player;
     FILE *arq;
-    int ind;
+    int ind, indPesquisa;
 
+    // Lidando com o arquivo
     arq = fopen("playerscopy.csv", "r");
 
     if(arq == NULL){
@@ -21,17 +24,18 @@ int main(){
         return 0;
     }
 
-    char linha[1000];
-    fgets(linha, 1000, arq);
+    char linha[100];
+    fgets(linha, 100, arq);
 
-    while(fgets(linha, 1000, arq) != NULL){
+    // Adicionando os dados à hashtable
+    while(fgets(linha, 100, arq) != NULL){
         char *token = strtok(linha, ",");
         player.id = atoi(token);
 
         ind = player.id % 50;
 
         token = strtok(NULL, ",");
-        player.team = token;
+        player.name = token;
 
         token = strtok(NULL, ",");
         player.position = token;
@@ -41,13 +45,24 @@ int main(){
 
     fclose(arq);
 
-    for(int i = 0; i < 100; i++){
-        cout << "Indice: " << i << endl;
-        for(int j = 0; j < hashtable[i].size(); j++){
-            cout << hashtable[i][j].id << " " << hashtable[i][j].team << " " << hashtable[i][j].position << endl;
-        }
-    }
+    // Pesquisando os dados
+    do{
+        cout << "Digite o id do jogador (-1 para parar): ";
+        cin >> indPesquisa;
+
+        buscaJogador(hashtable, indPesquisa);
+    }while(ind != -1);
 
     return 0;
 }
 
+void buscaJogador (vector<vector<Players>> hashtable, int id){
+    int ind = id % 50;
+
+    for(int i = 0; i < hashtable[ind].size(); i++){
+        if(hashtable[ind][i].id == id){
+            cout << "Nome do jogador: " << hashtable[ind][i].name << endl;
+            break;
+        }
+    }
+}
