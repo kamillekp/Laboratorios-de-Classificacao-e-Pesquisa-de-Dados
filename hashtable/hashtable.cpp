dfc16dc419088eb5b2b095e593de2ffe28acaa87
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2023-12-08 22:00:46 -0300

    deu por hj

--- hashtable/hashtable.cpp
+++ hashtable/hashtable.cpp
@@ -14,9 +14,15 @@ int main(){
     vector<vector<Players>> hashtable(50);
     Players player;
     FILE *arq;
-    int ind, indPesquisa;
 
-    // Lidando com o arquivo
+    char linha[100];
+    int ind;
+
+
+    //--------------------------------------------------------------------------------------------------------------------------------------
+    // Lendo os dados
+    //--------------------------------------------------------------------------------------------------------------------------------------
+
     arq = fopen("playerscopy.csv", "r");
 
     if(arq == NULL){
@@ -24,10 +30,8 @@ int main(){
         return 0;
     }
 
-    char linha[100];
-    fgets(linha, 100, arq);
-
     // Adicionando os dados à hashtable
+    fgets(linha, 100, arq);
     while(fgets(linha, 100, arq) != NULL){
         char *token = strtok(linha, ",");
         player.id = atoi(token);
@@ -45,24 +49,40 @@ int main(){
 
     fclose(arq);
 
-    // Pesquisando os dados
-    do{
-        cout << "Digite o id do jogador (-1 para parar): ";
-        cin >> indPesquisa;
 
-        buscaJogador(hashtable, indPesquisa);
-    }while(ind != -1);
+    //--------------------------------------------------------------------------------------------------------------------------------------
+    // Pesquisando os dados
+    //--------------------------------------------------------------------------------------------------------------------------------------
+
+    arq = fopen("consultascopy.csv", "r");
+
+    if(arq == NULL){
+        cout << "Erro ao abrir o arquivo" << endl;
+        return 0;
+    }
+
+    while(fgets(linha, 100, arq) != NULL){
+        ind = atoi(linha);
+        buscaJogador(hashtable, ind);
+    }
 
     return 0;
 }
 
 void buscaJogador (vector<vector<Players>> hashtable, int id){
     int ind = id % 50;
+    int flag = 0;
 
     for(int i = 0; i < hashtable[ind].size(); i++){
         if(hashtable[ind][i].id == id){
             cout << "Nome do jogador: " << hashtable[ind][i].name << endl;
+            flag = 1;
             break;
         }
     }
+
+    if(flag == 0){
+        cout << "Jogador nao encontrado" << endl;
+    }
+
 }
