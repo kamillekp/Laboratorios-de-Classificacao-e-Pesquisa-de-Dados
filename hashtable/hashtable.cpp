f6b9d53aa3f2454a56b61a5600cbb16e816b6051
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2023-12-09 16:55:14 -0300

    demorando muito na part 2 // ver

--- hashtable/hashtable.cpp
+++ hashtable/hashtable.cpp
@@ -1,30 +1,39 @@
 #include <bits/stdc++.h>
 
 using namespace std;
+
+#define MAX 997
 typedef struct{
     int id;
     string name;
     string position;
 } Players;
 
-void buscaJogador (vector<vector<Players>> hashtable, int id);
+Players buscaJogador (vector<vector<Players>> hashtable, int id, int *consultas);
 
 int main(){
-    // Declarando os dados
-    vector<vector<Players>> hashtable(50);
-    Players player;
-    FILE *arq;
 
+
+    //--------------------------------------------------------------------------------------------------------------------------------------
+    // Declarando os dados
+    //--------------------------------------------------------------------------------------------------------------------------------------
+
+    vector<vector<Players>> hashtable(MAX);
+    vector<Players> nomes;
+    vector<int> consult;
+    Players player;
+    FILE *arq, *saida;
+
+    int ind, ocupadas = 0, consultas;
     char linha[100];
-    int ind;
+    double tempoE, tempoP, start, end;
 
 
     //--------------------------------------------------------------------------------------------------------------------------------------
     // Lendo os dados
     //--------------------------------------------------------------------------------------------------------------------------------------
 
-    arq = fopen("playerscopy.csv", "r");
-
+    arq = fopen("players.csv", "r");
     if(arq == NULL){
         cout << "Erro ao abrir o arquivo" << endl;
         return 0;
@@ -32,11 +41,18 @@ int main(){
 
     // Adicionando os dados à hashtable
     fgets(linha, 100, arq);
+
+    start = clock();
     while(fgets(linha, 100, arq) != NULL){
         char *token = strtok(linha, ",");
         player.id = atoi(token);
 
-        ind = player.id % 50;
+        ind = player.id % MAX;
+
+        // contabilizando o número de posições ocupadas
+        if(hashtable[ind].size() == 0){
+            ocupadas++;
+        }
 
         token = strtok(NULL, ",");
         player.name = token;
@@ -46,43 +62,94 @@ int main(){
 
         hashtable[ind].push_back(player);
     }
+    end = clock();
 
     fclose(arq);
+
+    // calculando o tempo de execução da inserção
+    tempoE = (end - start) / (double) CLOCKS_PER_SEC;
+    tempoE *= 1000;
+
+    ocupadas = (float) ocupadas / MAX;    
 
 
     //--------------------------------------------------------------------------------------------------------------------------------------
     // Pesquisando os dados
     //--------------------------------------------------------------------------------------------------------------------------------------
 
-    arq = fopen("consultascopy.csv", "r");
-
+    arq = fopen("consultas.csv", "r");
     if(arq == NULL){
         cout << "Erro ao abrir o arquivo" << endl;
         return 0;
     }
 
+    // Pesquisando os dados na hashtable
+    start = clock();
     while(fgets(linha, 100, arq) != NULL){
+        consultas = 0;
         ind = atoi(linha);
-        buscaJogador(hashtable, ind);
+
+        player = buscaJogador(hashtable, ind, &consultas);
+
+        // adicionando os dados à lista de saída
+        nomes.push_back(player);
+        consult.push_back(consultas);
     }
+    end = clock();
+
+    fclose(arq);
+
+    // calculando o tempo de execução da pesquisa
+    tempoP = (end - start) / (double) CLOCKS_PER_SEC;
+    tempoP *= 1000;
+
+
+    //--------------------------------------------------------------------------------------------------------------------------------------
+    // saída do experimento
+    //--------------------------------------------------------------------------------------------------------------------------------------
+
+    saida = fopen("experimento997.txt", "w");
+    if(saida == NULL){
+        cout << "Erro ao abrir o arquivo" << endl;
+        return 0;
+    }
+
+    // printando as saídas 1
+    fprintf(saida, "PARTE1: ESTATISTICAS DA TABELA HASH\n");
+    fprintf(saida, "TEMPO DE CONSTRUCAO DA TABELA %lf EM MILISEGUNDOS\n", tempoE);
+    fprintf(saida, "TAXA DE OCUPACAO %.2f\n", ocupadas);
+    fprintf(saida, "TAMANHO MAXIMO DE LISTA #MAX1\n");      // FALTA
+    fprintf(saida, "TAMANHO MEDIO DE LISTA #MED1\n");       // FALTA
+
+    // printando as saídas 2
+    fprintf(saida, "\n\nPARTE2: ESTATISTICAS DA TABELA HASH\n");
+    fprintf(saida, "TEMPO DE CONSULTA %lf EM MILISEGUNDOS\n", tempoP);
+    for (int i = 0; i < consult.size(); i++){
+        if(nomes[i].id == -1){
+            fprintf(saida, "%d NAO ENCONTRADO\n", nomes[i].id);
+        }
+        else{
+            fprintf(saida, "%d %s %d\n", nomes[i].id, nomes[i].name.c_str(), consult[i]);
+        }
+    }
+    fprintf(saida, "MAXIMO NUMERO DE TESTES POR NOME ENCONTRADO #MAX2\n");      // FALTA
+    fprintf(saida, "MEDIA DE TESTES POR NOME ENCONTRADO #MED2\n");              // FALTA
+
+    fclose(saida);
 
     return 0;
 }
 
-void buscaJogador (vector<vector<Players>> hashtable, int id){
+Players buscaJogador (vector<vector<Players>> hashtable, int id, int *consultas){
     int ind = id % 50;
-    int flag = 0;
 
     for(int i = 0; i < hashtable[ind].size(); i++){
+        (*consultas)++;
+
         if(hashtable[ind][i].id == id){
-            cout << "Nome do jogador: " << hashtable[ind][i].name << endl;
-            flag = 1;
-            break;
+            return hashtable[ind][i];
         }
     }
 
-    if(flag == 0){
-        cout << "Jogador nao encontrado" << endl;
-    }
-
+    return {ind, "", ""}; 
 }
