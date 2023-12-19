e0f706f4893e308c3afbea76b7cbfcd41bfd8f2d
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2023-12-19 18:14:02 -0300

    entrada padrão alterada

--- hashtable/hashtable.cpp
+++ hashtable/hashtable.cpp
@@ -1,25 +1,24 @@
 #include <bits/stdc++.h>
 
 using namespace std;
-
-#define MAX 997 // fixo para criar os testes M = 997, 1999, 3989 e 7993
 typedef struct{
     int id;
     string name;
     string position;
 } Players;
 
-void insereJogador (vector<vector<Players>>& hashtable, Players player, int *ocupadas);
-Players buscaJogador (vector<vector<Players>>& hashtable, int id, int *consultas);
+void insereJogador (vector<vector<Players>>& hashtable, Players player, int *ocupadas, int MAX);
+Players buscaJogador (vector<vector<Players>>& hashtable, int id, int *consultas, int MAX);
 void printa_hashtable(vector<vector<Players>>& hashtable,int numLinhas);
 void printaPlayer(Players player);
 
-int main(){
+int main(int argc, char *argv[]){
 
     
     //--------------------------------------------------------------------------------------------------------------------------------------
     // Declarando os dados
     //--------------------------------------------------------------------------------------------------------------------------------------
+    int MAX = atoi(argv[1]);
 
     vector<vector<Players>> hashtable(MAX);
     vector<Players> nomes;
@@ -27,16 +26,16 @@ int main(){
     vector<int> consult;
     FILE *arq, *saida;
 
-    int id, ocupadas = 0, consultas;
+    int id, ocupadas = 0, consultas, start, end;
     char linha[100];
-    double tempoE, tempoP, start, end;
+    double tempoE, tempoP;
 
 
     //--------------------------------------------------------------------------------------------------------------------------------------
     // Lendo os dados
     //--------------------------------------------------------------------------------------------------------------------------------------
     
-    arq = fopen("players.csv", "r"); 
+    arq = fopen(argv[2], "r"); 
     if(arq == NULL){
         cout << "Erro ao abrir o arquivo" << endl;
         return 0;
@@ -56,14 +55,14 @@ int main(){
         token = strtok(NULL, "\"");
         player.position = token;
 
-        insereJogador(hashtable, player, &ocupadas);
+        insereJogador(hashtable, player, &ocupadas, MAX);
     }
     end = clock();
 
     fclose(arq);
 
     // calculando o tempo de execução da inserção
-    tempoE = (end - start) / (double) CLOCKS_PER_SEC;
+    tempoE = (double) (end - start) / CLOCKS_PER_SEC;
     tempoE *= 1000;
 
     ocupadas = (float) ocupadas / MAX;    
@@ -85,7 +84,7 @@ int main(){
         consultas = 0;
         id = atoi(linha);
 
-        player = buscaJogador(hashtable, id, &consultas);
+        player = buscaJogador(hashtable, id, &consultas, MAX);
 
         // adicionando os dados à lista de saída
         nomes.push_back(player);
@@ -96,7 +95,7 @@ int main(){
     fclose(arq);
 
     // calculando o tempo de execução da pesquisa
-    tempoP = (end - start) / (double) CLOCKS_PER_SEC;
+    tempoP = (double) (end - start) / CLOCKS_PER_SEC;
     tempoP *= 1000;
 
 
@@ -104,7 +103,11 @@ int main(){
     // saída do experimento
     //--------------------------------------------------------------------------------------------------------------------------------------
 
-    saida = fopen("experimento997.txt", "w");
+    char nomeArq[100];
+    strcpy(nomeArq, argv[1]);
+    strcat(nomeArq, ".txt");
+
+    saida = fopen(nomeArq, "w");
     if(saida == NULL){
         cout << "Erro ao abrir o arquivo" << endl;
         return 0;
@@ -112,14 +115,14 @@ int main(){
 
     // printando as saídas 1
     fprintf(saida, "PARTE1: ESTATISTICAS DA TABELA HASH\n");
-    fprintf(saida, "TEMPO DE CONSTRUCAO DA TABELA %lf EM MILISEGUNDOS\n", tempoE);
+    fprintf(saida, "TEMPO DE CONSTRUCAO DA TABELA %0.f MILISEGUNDOS\n", tempoE);
     fprintf(saida, "TAXA DE OCUPACAO %.2f\n", ocupadas);
     fprintf(saida, "TAMANHO MAXIMO DE LISTA #MAX1\n");      // FALTA
     fprintf(saida, "TAMANHO MEDIO DE LISTA #MED1\n");       // FALTA
 
     //printando as saídas 2
     fprintf(saida, "\n\nPARTE2:  ESTATISTICAS DAS CONSULTAS\n");
-    fprintf(saida, "TEMPO DE CONSULTA %lf EM MILISEGUNDOS\n", tempoP);
+    fprintf(saida, "TEMPO DE CONSULTA %0.f EM MILISEGUNDOS\n", tempoP);
     for (int i = 0; i < consult.size(); i++){
         fprintf(saida, "%d %s %d\n", nomes[i].id, nomes[i].name.c_str(), consult[i]);
     }
@@ -131,7 +134,7 @@ int main(){
     return 0;
 }
 
-void insereJogador (vector<vector<Players>>& hashtable, Players player, int *ocupadas){
+void insereJogador (vector<vector<Players>>& hashtable, Players player, int *ocupadas, int MAX){
     int ind = player.id % MAX;
 
     if(hashtable[ind].size() == 0){
@@ -141,7 +144,7 @@ void insereJogador (vector<vector<Players>>& hashtable, Players player, int *ocu
     hashtable[ind].push_back(player); // esta acontecendo o push_back
 }
 
-Players buscaJogador (vector<vector<Players>>& hashtable, int id, int *consultas){
+Players buscaJogador (vector<vector<Players>>& hashtable, int id, int *consultas, int MAX){
     int ind = id % MAX;
 
     for(int i = 0; i < hashtable[ind].size(); i++){
