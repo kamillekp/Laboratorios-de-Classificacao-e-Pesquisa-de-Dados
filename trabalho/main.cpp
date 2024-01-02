90b390888e76ef6c5eb65dcc7d20439a8e2a7d21
Kamille Konarzewski <kamille.kpimentel@gmail.com>
2024-01-02 18:55:31 -0300

    hash para tags pronta

--- trabalho/main.cpp
+++ trabalho/main.cpp
@@ -1,12 +1,14 @@
 #include <bits/stdc++.h>  
 #include "hashtable_Player.h" 
 #include "hashtable_User.h" 
+#include "hashtable_Tag.h"
 
 using namespace std;
 
 int main(int argc, char *argv[]){
-    string id, id_user, short_name, long_name, position, nacionality, club, league, rating;
+    string id, id_user, short_name, long_name, position, nacionality, club, league, rating, tag;
     string line;
+    ifstream file;
 
     //---------------------------------------------------------------------------------------------------------
     // ENTRADA PADRÃO DO ARQUIVO PRINCIPAL COM OS DADOS DOS JOGADORES
@@ -36,11 +38,11 @@ int main(int argc, char *argv[]){
 
     //hashtable_player.print_hashtable();
 
+
     //---------------------------------------------------------------------------------------------------------
-    //ENTRADA DO ARQUIVO COM AVAALIAÇÕES DOS USUÁRIOS
+    //ENTRADA DO ARQUIVO COM AVALIAÇÕES DOS USUÁRIOS
     Hashtable_User hashtable_user(200);
 
-    ifstream file;
     file.open(argv[1]);
 
     if(!file.is_open()){
@@ -54,12 +56,41 @@ int main(int argc, char *argv[]){
 
         getline(ss, id_user, ',');
         getline(ss, id, ',');
-        getline(ss, rating, ',');
+        getline(ss, tag, ',');
 
-        hashtable_user.insert_user(User(id_user, id, rating));
+        hashtable_user.insert_user(User(id_user, id, tag));
     }
 
+    file.close();
+
     //hashtable_user.print_hashtable();
+
+
+    //---------------------------------------------------------------------------------------------------------
+    // ENTRADA DO ARQUIVO COM AS CONSULTAS USANDO TAGS
+    Hashtable_Tag hashtable_tag(200);
+
+    file.open(argv[2]);
+
+    if(!file.is_open()){
+        cout << "Arquivo nao encontrado." << endl;
+        return 0;
+    }
+
+    getline(file, line);
+    while(getline(file, line)){
+        stringstream ss(line);
+
+        getline(ss, id_user, ',');
+        getline(ss, id, ',');
+        getline(ss, tag, ',');
+
+        hashtable_tag.insert_tag(Tag(id_user, id, tag));
+    }
+
+    file.close();
+
+    hashtable_tag.print_hashtable();
 
     return 0;
 }
---
+++ trabalho/tag.h
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Tag {
+    public:
+        string id;
+        string id_player;
+        string tag;
+
+        Tag(string id, string id_player, string tag){
+            this->id = id;
+            this->id_player = id_player;
+            this->tag = tag;
+        }
+};
+
